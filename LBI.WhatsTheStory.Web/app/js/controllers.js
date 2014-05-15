af50d58893d27c3e9a803826c442033774fb23d8
Samuel Nissim <samnissim@gmail.com>
2014-05-15 16:58:29 -0500

    added visualization for daily stock quotes

--- LBI.WhatsTheStory.Web/app/js/controllers.js
+++ LBI.WhatsTheStory.Web/app/js/controllers.js
@@ -1,10 +1,51 @@
-﻿var storyControllers = angular.module('storyControllers', []);
+﻿google.load('visualization', '1', { packages: ['corechart'] });
+
+google.setOnLoadCallback(function () {
+    angular.bootstrap(document.body, ['storyApp']);
+});
+
+var storyControllers = angular.module('storyControllers', []);
+
+function parseDate(dotnetDate) {
+    return new Date(parseInt(dotnetDate.substr(6)));
+}
 
 function formatDate(dotnetDate) {
-    var date = new Date(parseInt(dotnetDate.substr(6)));
+    var date = parseDate(dotnetDate);
     var month = ("0" + (date.getMonth() + 1));
     var day = "0" + date.getDate();
     return date.getFullYear() + "-" + month.substr(month.length - 2) + "-" + day.substr(day.length - 2);
+}
+
+function drawVisualization(data) {
+/*
+    var data = google.visualization.arrayToDataTable([
+          ['Mon', 20, 28, 38, 45],
+          ['Tue', 31, 38, 55, 66],
+          ['Wed', 50, 55, 77, 80],
+          ['Thu', 77, 77, 66, 50],
+          ['Fri', 68, 66, 22, 15]
+    // Treat first row as data as well.
+        ], true);
+*/
+
+    var datatable = new google.visualization.DataTable();
+    datatable.addColumn('date');
+    datatable.addColumn('number');
+    datatable.addColumn('number');
+    datatable.addColumn('number');
+    datatable.addColumn('number');
+
+    $.each(data, function (i, quote) {
+        datatable.addRow([parseDate(quote.Date), quote.Low, quote.Open, quote.Close, quote.High]);
+    });
+
+    var options = {
+        legend: 'none'
+    };
+
+    var chart = new google.visualization.CandlestickChart(document.getElementById('stock-chart'));
+    chart.draw(datatable, options);
 }
 
 storyControllers.controller('DashboardCtrl', function ($scope, $http, $log, $modal, $location, $routeParams) {
@@ -20,14 +61,27 @@ storyControllers.controller('DashboardCtrl', function ($scope, $http, $log, $mod
         }
     });
 
-    $scope.setClient = function (clientId) {
+    $scope.stockPrice_show = function () {
+        var clientId = $("#client-select").val();
         $http.get('/data/stockprices/' + clientId).success(function (data) {
             //            console.log('got new data');
-            $.each(data, function (i, quote) {
-                data[i].ParsedDate = formatDate(quote.Date);
-            });
-            $scope.quotes = data;
+//            $.each(data, function (i, quote) {
+//                data[i].ParsedDate = formatDate(quote.Date);
+//            });
+            //$scope.quotes = data;
+            drawVisualization(data);
         });
+
+    }
+
+    $scope.setClient = function (clientId) {
+        //        $http.get('/data/stockprices/' + clientId).success(function (data) {
+        //            //            console.log('got new data');
+        //            $.each(data, function (i, quote) {
+        //                data[i].ParsedDate = formatDate(quote.Date);
+        //            });
+        //            $scope.quotes = data;
+        //        });
 
         $http.get('/data/awardaverages/' + clientId).success(function (data) {
             $scope.awardAverages = data;
